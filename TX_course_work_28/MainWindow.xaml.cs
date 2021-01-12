2163b4cf01037ca8b85f24900117e9a7b5091c69
Yulia <yu_ant@list.ru>
2021-01-12 13:06:13 +0300

    Кривые окна

--- TX_course_work_28/MainWindow.xaml.cs
+++ TX_course_work_28/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using TX_course_work_28.NewFolder1;
 
 namespace TX_course_work_28
 {
@@ -20,9 +23,76 @@ namespace TX_course_work_28
     /// </summary>
     public partial class MainWindow : Window
     {
+        int Mode;
+        Exhibitions DailyExhibitions;
+
+
         public MainWindow()
         {
+            Mode = 0;
+            DailyExhibitions = new Exhibitions(Mode);
             InitializeComponent();
+            this.DataContext = DailyExhibitions;
+            ListOfExhibitions.ItemsSource = DailyExhibitions.exh;
+            ListwithInformation.ItemsSource = DailyExhibitions.exh;
+            ListOfExhibitions.Items.Refresh();
+            ListwithInformation.Items.Refresh();
+        }
+
+        private void ChooseFileClick_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void ChangeTheHall_Click(object sender, RoutedEventArgs e)
+        {
+            
+           ListOfExhibitions.ItemsSource = DailyExhibitions.exh;
+            switch (Mode)
+            {
+                case 0:
+                    {
+                       
+                        DailyExhibitions = new Exhibitions(Mode);
+                        ListwithInformation.ItemsSource = DailyExhibitions.exh;
+                        Mode = 1;
+                        break;
+                    }
+                case 1:
+                    {
+                       
+                        DailyExhibitions = new Exhibitions(Mode);
+                        ListwithInformation.ItemsSource = DailyExhibitions.exh;
+                        Mode = 2;
+                        break;
+                    }
+                case 2:
+                    {
+                       
+                        DailyExhibitions = new Exhibitions(Mode);
+                        ListwithInformation.ItemsSource = DailyExhibitions.exh;
+                        Mode =3 ;
+                        break;
+                    }
+                case 3:
+                    {
+                        
+                        DailyExhibitions = new Exhibitions(Mode);
+                        ListwithInformation.ItemsSource = DailyExhibitions.exh;
+                        Mode = 4;
+                        break;
+                    }
+                case 4:
+                    {
+                        
+                        DailyExhibitions = new Exhibitions(Mode);
+                        ListwithInformation.ItemsSource = DailyExhibitions.exh;
+                        Mode=0;
+                        break;
+                    }
+               
+
+            }
         }
     }
 }
---
+++ TX_course_work_28/NewFolder1/interfacee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TX_course_work_28.NewFolder1
+{
+    interface interfacee
+    {
+        string room { get; set; }
+         string name { get; set; }
+        string information { get; set; }
+    }
+}
